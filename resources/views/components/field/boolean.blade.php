9e4f74cc5437f0b8b55c72f9fefb13e0db8f558d
Kevin Dierkx <kevin@distortedfusion.com>
2021-03-11 21:31:47 +0100

    Replaced disabled icon

--- resources/views/components/field/boolean.blade.php
+++ resources/views/components/field/boolean.blade.php
@@ -1,5 +1,5 @@
 @if ($state)
     <i class="far fa-fw fa-check-circle text-success"></i>
 @else
-    <i class="far fa-fw fa-cross-circle text-danger"></i>
+    <i class="far fa-fw fa-times-circle text-danger"></i>
 @endif
