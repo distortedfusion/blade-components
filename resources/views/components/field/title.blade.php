fdc299edb026bb91da49f7d74e504effbd4b440c
Kevin Dierkx <kevin@distortedfusion.com>
2021-03-17 19:51:02 +0100

    Added truncate

--- resources/views/components/field/title.blade.php
+++ resources/views/components/field/title.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div {{ $attributes->merge(['class' => 'truncate']) }}>
     <span class="text-sm font-bold leading-5">{{ $title }}</span>
     @if ($subTitle)
         <div class="text-muted text-xs font-medium leading-5">
