f483d3e02f6425e9f1a5cbafaf4fe774eda0407c
Kevin Dierkx <kevin@distortedfusion.com>
2021-09-22 10:25:03 +0200

    Added button slots to actions component

--- resources/views/components/form/actions.blade.php
+++ resources/views/components/form/actions.blade.php
@@ -1,12 +1,20 @@
 <x-form.section>
     <div class="col-span-3">
-        <a href="{{ $cancelUrl() }}" class="btn btn-block btn-default">
-            {{ $cancelTitle ?: __('Cancel') }}
-        </a>
+        @if ($cancelButton ?? false)
+            {{ $cancelButton }}
+        @else
+            <a href="{{ $cancelUrl() }}" class="btn btn-block btn-default">
+                {{ $cancelTitle ?: __('Cancel') }}
+            </a>
+        @endif
     </div>
     <div class="col-span-3">
-        <x-form-submit :class="sprintf('btn-block %s', $submitButtonClass())">
-            {{ $submitTitle }}
-        </x-form-submit>
+        @if ($submitButton ?? false)
+            {{ $submitButton }}
+        @else
+            <x-form-submit :class="sprintf('btn-block %s', $submitButtonClass())">
+                {{ $submitTitle }}
+            </x-form-submit>
+        @endif
     </div>
 </x-form.section>
