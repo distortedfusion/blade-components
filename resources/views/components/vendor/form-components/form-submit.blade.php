8f1990506ff19ccf22ed8e11883e17d5f47f77c3
Kevin Dierkx <kevin@distortedfusion.com>
2022-01-24 18:30:21 +0100

    Spinny submit button

--- resources/views/components/vendor/form-components/form-submit.blade.php
+++ resources/views/components/vendor/form-components/form-submit.blade.php
@@ -1,8 +1,18 @@
 <div class="flex items-center justify-between">
     <button {!! $attributes->merge([
-        'class' => Str::contains($attributes->get('class'), array_keys(\DistortedFusion\Tailwind\Concerns\ButtonStyling::buttonStyles())) ? 'btn' : 'btn btn-primary',
-        'type' => 'submit'
-    ]) !!}>
-        {!! trim($slot) ?: __('Submit') !!}
+            'class' => Str::contains($attributes->get('class'), array_keys(\DistortedFusion\Tailwind\Concerns\ButtonStyling::buttonStyles())) ? 'btn' : 'btn btn-primary',
+            'type' => 'submit'
+        ]) !!}
+        @if($isWired())
+        wire:loading.attr="disabled"
+        @endif>
+        @if($isWired())
+            <div class="hidden" wire:loading.block>
+                <x-spinner class="p-0" />
+            </div>
+        @endif
+        <div @if($isWired())wire:loading.class="hidden"@endif>
+            {!! trim($slot) ?: __('Submit') !!}
+        </div>
     </button>
 </div>
