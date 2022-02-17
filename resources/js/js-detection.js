4d8e6c2d6e82a35e7c7e998bc55de0b4074105e8
Kevin Dierkx <kevin@distortedfusion.com>
2022-02-17 13:13:30 +0100

    Added basic sidebar toggle script and js detection script

---
+++ resources/js/js-detection.js
@@ -0,0 +1,4 @@
+// Removes the `.no-js` class from the html element and replaces it with `.js`.
+// This is used for some elements to determine JS support and respond accordingly.
+let html = document.getElementsByTagName("html")[0];
+html.className = html.className.replace(/\bno-js\b/, 'js');
---
+++ resources/js/sidebar.js
@@ -0,0 +1,13 @@
+import $ from 'jquery'
+
+window.addEventListener('load', function () {
+    $('#sidebarDismiss, #sidebarBackdrop').on('click', function () {
+        $('#sidebar').removeClass('show');
+        $('#sidebarBackdrop').removeClass('show');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('show');
+        $('#sidebarBackdrop').addClass('show');
+    });
+});
