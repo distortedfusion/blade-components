bd5c4bcf2fa44767700f6d0cec23d74655f0bbad
Kevin Dierkx <kevin@distortedfusion.com>
2022-02-21 14:39:50 +0100

    Removed base from app-base layout

--- resources/views/components/layouts/app-base.blade.php
+++ resources/views/components/layouts/app-base.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <base href="/">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
