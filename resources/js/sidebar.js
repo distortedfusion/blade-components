9582490adad519e1054a4f1064659b008dbcf442
Kevin Dierkx <kevin@distortedfusion.com>
2022-02-21 12:46:56 +0100

    Sidebar class and toggle addition

--- resources/js/sidebar.js
+++ resources/js/sidebar.js
@@ -4,10 +4,12 @@ window.addEventListener('load', function () {
     $('#sidebarDismiss, #sidebarBackdrop').on('click', function () {
         $('#sidebar').removeClass('show');
         $('#sidebarBackdrop').removeClass('show');
+        $('#sidebarCollapse').removeClass('in');
     });
 
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').addClass('show');
         $('#sidebarBackdrop').addClass('show');
+        $('#sidebarCollapse').addClass('in');
     });
 });
