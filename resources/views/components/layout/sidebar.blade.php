8b53cc85cedc486bf85ea6f54a952b82f4afe0a4
Kevin Dierkx <kevin@distortedfusion.com>
2022-02-21 11:53:03 +0100

    Attributes support for sidebar

--- resources/views/components/layout/sidebar.blade.php
+++ resources/views/components/layout/sidebar.blade.php
@@ -1,4 +1,7 @@
-<div id="sidebar" class="fixed top-0 lg:left-0 w-64 h-full z-modal lg:z-auto">
+<div {{ $attributes->merge(['id' => 'sidebar'])->class([
+    'fixed top-0 lg:left-0 w-64 h-full',
+    'z-modal' => ! Str::contains($attributes->get('class'), 'z-'),
+]) }}>
     {{ $slot }}
 </div>
 
