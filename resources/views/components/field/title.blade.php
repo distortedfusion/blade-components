5f13df75f56cdcc8fcca5a66ede125b1a69f10ae
Kevin Dierkx <kevin@distortedfusion.com>
2022-07-01 18:39:51 +0200

    Fixed sub-title styling

--- resources/views/components/field/title.blade.php
+++ resources/views/components/field/title.blade.php
@@ -1,11 +1,11 @@
+@props(['subTitle'])
+
 <div {{ $attributes->class(['truncate' => $truncate]) }}>
     <span class="text-sm font-bold leading-5">{!! $title ?: $slot !!}</span>
     @if ($subTitle)
-        @php
-        $subTitleAttributes = new \Illuminate\View\ComponentAttributeBag();
-        @endphp
-        <div {{ $subTitleAttributes->class([
-            'text-muted text-xs font-medium leading-5',
+        <div {{ $subTitle->attributes->class([
+            'text-xs font-medium leading-5',
+            'text-muted' => ! Str::contains($subTitle->attributes->get('class'), 'text-'),
             'truncate' => $truncate,
         ]) }}>
             {!! $subTitle !!}
