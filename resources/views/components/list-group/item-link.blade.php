1d5ec3a2c1c53eb98fd52495ad20949612e7f573
Kevin Dierkx <kevin@distortedfusion.com>
2022-08-04 16:55:50 +0200

    Improved list-item alignment

--- resources/views/components/list-group/item-link.blade.php
+++ resources/views/components/list-group/item-link.blade.php
@@ -1,13 +1,16 @@
 <a href="{{ $url }}" class="list-group-item hover:no-underline hover:bg-gray-50">
-    <div class="flex items-baseline">
-        @if(! is_null($title))
-            <div class="w-1/3 flex-shrink-0">
-                <h3 class="text-xs text-muted font-semibold">{!! $title !!}</h3>
-            </div>
-        @endif
-        <div class="flex-grow">{!! $slot ?: '&mdash;' !!}</div>
+    <div class="flex items-center">
+        <div class="flex-grow flex items-baseline">
+            @if(! is_null($title))
+                <div class="w-1/3 flex-shrink-0">
+                    <h3 class="text-xs text-muted font-semibold">{!! $title !!}</h3>
+                </div>
+            @endif
+            <div class="flex-grow">{!! $slot ?: '&mdash;' !!}</div>
+        </div>
+
         <div class="flex-no-shrink">
-            <i class="fal fa-fw fa-chevron-right text-muted"></i>
+            <x-fas-angle-right class="text-muted" />
         </div>
     </div>
 </a>
