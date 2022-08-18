3dcd769cf7ef5ce00dcd7c228f87aa2567c556d4
Kevin Dierkx <kevin@distortedfusion.com>
2022-08-18 16:07:22 +0200

    removed leading and text size from field.title

--- resources/views/components/field/title.blade.php
+++ resources/views/components/field/title.blade.php
@@ -1,10 +1,9 @@
 @props(['subTitle'])
 
 <div {{ $attributes->class(['truncate' => $truncate]) }}>
-    <span class="text-sm font-bold leading-5">{!! $title ?: $slot !!}</span>
+    <span class="font-bold">{!! $title ?: $slot !!}</span>
     @if ($subTitle)
         <div {{ $subTitle->attributes->class([
-            'text-xs font-medium leading-5',
             'text-muted' => ! Str::contains($subTitle->attributes->get('class'), 'text-'),
             'truncate' => $truncate,
         ]) }}>
