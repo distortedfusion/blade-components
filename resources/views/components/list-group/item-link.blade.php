34a6a04448c3b2d2421e73f75a00e20057a36fde
Kevin Dierkx <kevin@distortedfusion.com>
2022-08-19 16:22:59 +0200

    Fixed missing comma

--- resources/views/components/list-group/item-link.blade.php
+++ resources/views/components/list-group/item-link.blade.php
@@ -1,7 +1,7 @@
 <a href="{{ $url }}" {{ $attributes->class([
     'list-group-item block relative border',
     'text-current hover:no-underline',
-    'hover:bg-gray-100' => ! Str::contains($attributes->get('class'), 'hover:bg-')
+    'hover:bg-gray-100' => ! Str::contains($attributes->get('class'), 'hover:bg-'),
     'sm:px-6' => ! Str::contains($attributes->get('class'), ['sm:px-', 'sm:pl-', 'sm:pr-']),
     'px-4' => ! Str::contains($attributes->get('class'), ['px-', 'pl-', 'pr-']),
     'py-4' => ! Str::contains($attributes->get('class'), ['py-', 'pt-', 'pb-']),
