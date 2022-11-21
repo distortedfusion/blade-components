e189f255f10a580978448ebc77a1c00ebb50f6b0
Kevin Dierkx <kevin@distortedfusion.com>
2022-11-22 00:07:59 +0100

    Updated list-group.item-link/item-button styling

--- resources/views/components/list-group/item-button.blade.php
+++ resources/views/components/list-group/item-button.blade.php
@@ -1,19 +1,22 @@
-<button type="{{ $type }}" {{ $attributes->class([
-    'group list-group-item block relative border',
-    'text-current hover:no-underline',
-    'hover:bg-gray-100' => ! Str::contains($attributes->get('class'), 'hover:bg-'),
+<div {{ $attributes->class([
+    'list-group-item block relative border-t border-b',
     'sm:px-6' => ! Str::contains($attributes->get('class'), ['sm:px-', 'sm:pl-', 'sm:pr-']),
     'px-4' => ! Str::contains($attributes->get('class'), ['px-', 'pl-', 'pr-']),
     'py-4' => ! Str::contains($attributes->get('class'), ['py-', 'pt-', 'pb-']),
 ]) }}>
-    <div class="flex items-center space-x-2">
-        <div class="flex-grow flex items-baseline">
+    <button type="{{ $type }}" class="{{
+        'group block rounded relative'
+        . ' -mx-4 px-4 -my-3 py-3'
+        . ' text-current hover:no-underline'
+        . ' hover:bg-gray-100'
+    }}">
+        <div class="flex items-baseline">
             @include('tailwind-layout::components.list-group.partials.title')
-            <div class="flex-grow text-{{ $align }}">{!! trim($slot) ? $slot : '&mdash;' !!}</div>
+            <div class="flex-grow text-{{ $align }} pr-4">{!! trim($slot) ? $slot : '&mdash;' !!}</div>
         </div>
 
-        <div class="flex-no-shrink">
-            @include('tailwind-layout::components.list-group.partials.icon')
+        <div class="absolute inset-y-1/2 right-4 -mt-2">
+            <x-fas-angle-right class="text-muted opacity-50 group-hover:opacity-100" />
         </div>
-    </div>
-</button>
+    </button>
+</div>
--- resources/views/components/list-group/partials/icon.blade.php
+++
@@ -1,5 +0,0 @@
-@if(is_null($icon))
-    <x-fas-angle-right class="text-muted" />
-@else
-    {!! $icon !!}
-@endif
