1643e49480a76b914cae83b57e5dac3ff01d7e95
Kevin Dierkx <kevin@distortedfusion.com>
2022-11-30 22:46:14 +0100

    Use same padding for list-group-item

--- resources/views/components/list-group/item.blade.php
+++ resources/views/components/list-group/item.blade.php
@@ -1,11 +1,15 @@
 <div {{ $attributes->class([
     'list-group-item block relative border-t border-b border-l border-r',
-    'sm:px-6' => ! Str::contains($attributes->get('class'), ['sm:px-', 'sm:pl-', 'sm:pr-']),
     'px-4' => ! Str::contains($attributes->get('class'), ['px-', 'pl-', 'pr-']),
     'py-4' => ! Str::contains($attributes->get('class'), ['py-', 'pt-', 'pb-']),
 ]) }}>
-    <div class="flex items-baseline">
-        @include('tailwind-layout::components.list-group.partials.title')
-        <div class="flex-grow text-{{ $align }}">{!! trim($slot) ? $slot : '&mdash;' !!}</div>
+    <div class="{{
+        'group block w-full rounded relative'
+        . ' px-2 sm:px-4 -my-3 py-3'
+    }}">
+        <div class="flex items-baseline">
+            @include('tailwind-layout::components.list-group.partials.title')
+            <div class="flex-grow text-{{ $align }}">{!! trim($slot) ? $slot : '&mdash;' !!}</div>
+        </div>
     </div>
 </div>
