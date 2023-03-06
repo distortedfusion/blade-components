8e40787d24cc152ac5f3525d48079c90b71fdc6c
Kevin Dierkx <kevin@distortedfusion.com>
2023-03-06 17:17:56 +0100

    Added a bytes formatter

---
+++ src/Concerns/FormatsSize.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DistortedFusion\Tailwind\Concerns;
+
+trait FormatsSize
+{
+    /**
+     * Formats bytes to a human readable format.
+     *
+     * @param string $bytes
+     *
+     * @return string
+     */
+    public static function formatBytes(int $bytes): string
+    {
+        $si_prefix = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        $class = min((int) log($bytes, 1024), count($si_prefix) - 1);
+
+        return sprintf('%1.2f', $bytes / pow(1024, $class)).' '.$si_prefix[$class];
+    }
+}
