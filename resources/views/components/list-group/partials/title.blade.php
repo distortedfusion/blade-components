506d598008c34cb7f5964179eea119513071d96e
Kevin Dierkx <kevin@distortedfusion.com>
2023-04-19 17:20:25 +0200

    Center align list-item title contents

--- resources/views/components/list-group/partials/title.blade.php
+++ resources/views/components/list-group/partials/title.blade.php
@@ -1,6 +1,7 @@
 @if(! is_null($title))
     <div class="flex-shrink-0{{ $align !== 'right' && ! ($vertical ?? false) ? ' w-1/3' : null }}">
         <h3 {{ $title->attributes->class([
+            'flex items-center',
             'text-xs text-muted font-semibold',
         ]) }}>{!! $title !!}</h3>
     </div>
