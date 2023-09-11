bf16401f0f3048c15bd71a51eb4b2369aa61dd3b
Kevin Dierkx <kevin@distortedfusion.com>
2023-09-11 19:35:27 +0200

    Text docs

---
+++ docs/20-text.md
@@ -0,0 +1,18 @@
+# Text
+
+## Heading
+
+```blade-component
+<x-layout.heading :heading-level="1">The Evil Rabbit Jumps.</x-layout.heading>
+<x-layout.heading :heading-level="2">The Evil Rabbit Jumps.</x-layout.heading>
+<x-layout.heading :heading-level="3">The Evil Rabbit Jumps.</x-layout.heading>
+<x-layout.heading :heading-level="4">The Evil Rabbit Jumps.</x-layout.heading>
+<x-layout.heading :heading-level="5">The Evil Rabbit Jumps.</x-layout.heading>
+<x-layout.heading :heading-level="6">The Evil Rabbit Jumps.</x-layout.heading>
+```
+
+## Codeblock
+
+```blade-component
+<x-pre>composer require distortedfusion/blade-components</x-pre>
+```
--- resources/views/components/layout/heading.blade.php
+++ resources/views/components/layout/heading.blade.php
@@ -1,4 +1,12 @@
 <h{{ $headingLevel }} {{ $attributes->merge(['id' => $id()])->class([
-    'font-bold hyphens-auto',
-    'text-4xl md:text-4xl' => ! Str::contains($attributes->get('class'), ['text-'])
+    'font-sans-heading',
+    'hyphens-auto' => ! Str::contains($attributes->get('class'), ['hyphens-']),
+    'font-black' => $headingLevel === 1 && ! Str::contains($attributes->get('class'), ['font-']),
+    'font-bold' => $headingLevel !== 1 && ! Str::contains($attributes->get('class'), ['font-']),
+    'text-5xl md:text-5xl' => $headingLevel === 1 && ! Str::contains($attributes->get('class'), ['text-']),
+    'text-4xl md:text-4xl' => $headingLevel === 2 && ! Str::contains($attributes->get('class'), ['text-']),
+    'text-3xl md:text-3xl' => $headingLevel === 3 && ! Str::contains($attributes->get('class'), ['text-']),
+    'text-2xl md:text-2xl' => $headingLevel === 4 && ! Str::contains($attributes->get('class'), ['text-']),
+    'text-xl md:text-xl' => $headingLevel === 5 && ! Str::contains($attributes->get('class'), ['text-']),
+    'text-lg md:text-lg' => $headingLevel === 6 && ! Str::contains($attributes->get('class'), ['text-']),
 ]) }}>{{ $slot }}</h{{ $headingLevel }}>
