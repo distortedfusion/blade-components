5b95e3e19fce78f66fed9bf6897e2f9cbfb8324b
Kevin Dierkx <kevin@distortedfusion.com>
2023-09-20 23:26:42 +0200

    Redesigned buttons

--- resources/views/components/btn/index.blade.php
+++ resources/views/components/btn/index.blade.php
@@ -1,6 +1,6 @@
 @php
 $class = [
-    'inline-flex items-center justify-center space-x-2 shrink-0 text-center border rounded-lg shadow transition-all',
+    'inline-flex items-center justify-center space-x-2 shrink-0 text-center border rounded-lg transition-all',
     'hover:no-underline hover:outline-0',
     'focus:no-underline focus:outline-0',
 
@@ -10,34 +10,40 @@ $class = [
     'h-8 py-0 px-4 text-sm font-semibold leading-8' => $size === 'sm',
 
     // Primary color...
-    'bg-brand-600 border-brand-800 text-white' => $style === 'primary',
-    'hover:bg-brand-500 hover:border-brand-500 hover:text-white' => $style === 'primary',
-    'focus:bg-brand-500 focus:border-brand-500 focus:text-white' => $style === 'primary',
-    'active:bg-brand-500 active:border-brand-500 active:text-white' => $style === 'primary',
+    'shadow-none bg-brand-600 border-brand-800 text-white' => $style === 'primary',
+    'shadow-none hover:bg-brand-500 hover:border-brand-500 hover:text-white' => $style === 'primary',
+    'shadow-none focus:bg-brand-500 focus:border-brand-500 focus:text-white' => $style === 'primary',
+    'shadow-none active:bg-brand-500 active:border-brand-500 active:text-white' => $style === 'primary',
 
-    // Default color...
-    'bg-white border-gray-300 text-gray-700' => $style === 'default',
-    'hover:bg-white hover:border-gray-300 hover:text-gray-500' => $style === 'default',
-    'focus:bg-white focus:border-gray-600 focus:text-gray-500' => $style === 'default',
-    'active:bg-gray-100 active:border-gray-600 active:text-gray-800' => $style === 'default',
+    // Secondary color...
+    'shadow-none bg-white border-gray-300 text-gray-700' => $style === 'secondary' || $style === 'default',
+    'shadow-none hover:bg-gray-100 hover:border-gray-300 hover:text-gray-700' => $style === 'secondary' || $style === 'default',
+    'shadow-none focus:bg-gray-100 focus:border-gray-300 focus:text-gray-700' => $style === 'secondary' || $style === 'default',
+    'shadow-none active:bg-gray-100 active:border-gray-300 active:text-gray-700' => $style === 'secondary' || $style === 'default',
+
+    // Tertiary color...
+    'shadow-none bg-transparent border-transparent text-gray-700' => $style === 'tertiary',
+    'shadow-none hover:bg-gray-100 hover:border-gray-100 hover:text-gray-700' => $style === 'tertiary',
+    'shadow-none focus:bg-gray-100 focus:border-gray-100 focus:text-gray-700' => $style === 'tertiary',
+    'shadow-none active:bg-gray-100 active:border-gray-100 active:text-gray-700' => $style === 'tertiary',
 
     // Info color...
-    'bg-blue-600 border-blue-600 text-black' => $style === 'info',
-    'hover:bg-blue-500 hover:border-blue-500 hover:text-black' => $style === 'info',
-    'focus:bg-blue-500 focus:border-blue-500 focus:text-black' => $style === 'info',
-    'active:bg-blue-500 active:border-blue-500 active:text-black' => $style === 'info',
+    'shadow-none bg-blue-600 border-blue-600 text-white' => $style === 'info',
+    'shadow-none hover:bg-blue-500 hover:border-blue-500 hover:text-white' => $style === 'info',
+    'shadow-none focus:bg-blue-500 focus:border-blue-500 focus:text-white' => $style === 'info',
+    'shadow-none active:bg-blue-500 active:border-blue-500 active:text-white' => $style === 'info',
 
     // Warning color...
-    'bg-amber-600 border-amber-600 text-black' => $style === 'warning',
-    'hover:bg-amber-500 hover:border-amber-500 hover:text-black' => $style === 'warning',
-    'focus:bg-amber-500 focus:border-amber-500 focus:text-black' => $style === 'warning',
-    'active:bg-amber-500 active:border-amber-500 active:text-black' => $style === 'warning',
+    'shadow-none bg-amber-600 border-amber-600 text-white' => $style === 'warning',
+    'shadow-none hover:bg-amber-500 hover:border-amber-500 hover:text-white' => $style === 'warning',
+    'shadow-none focus:bg-amber-500 focus:border-amber-500 focus:text-white' => $style === 'warning',
+    'shadow-none active:bg-amber-500 active:border-amber-500 active:text-white' => $style === 'warning',
 
     // Danger color...
-    'bg-red-600 border-red-600 text-black' => $style === 'danger',
-    'hover:bg-red-500 hover:border-red-500 hover:text-black' => $style === 'danger',
-    'focus:bg-red-500 focus:border-red-500 focus:text-black' => $style === 'danger',
-    'active:bg-red-500 active:border-red-500 active:text-black' => $style === 'danger',
+    'shadow-none bg-red-600 border-red-600 text-white' => $style === 'danger',
+    'shadow-none hover:bg-red-500 hover:border-red-500 hover:text-white' => $style === 'danger',
+    'shadow-none focus:bg-red-500 focus:border-red-500 focus:text-white' => $style === 'danger',
+    'shadow-none active:bg-red-500 active:border-red-500 active:text-white' => $style === 'danger',
 
     // Disabled color...
     'disabled:bg-white disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-not-allowed',
