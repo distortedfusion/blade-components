6652440b86f1f703273329000e70fbccd041941e
Kevin Dierkx <kevin@distortedfusion.com>
2023-09-29 00:37:03 +0200

    bar

--- resources/views/components/layout/progress-bar.blade.php
+++ resources/views/components/layout/progress-bar.blade.php
@@ -1,7 +1,7 @@
 <div {{ $attributes->class([
-    'w-4' => $size === 'lg',
-    'w-2' => is_null($size) || ! in_array($size, ['sm', 'lg']),
-    'w-1' => $size === 'sm',
+    'h-4' => $size === 'lg',
+    'h-2' => is_null($size) || ! in_array($size, ['sm', 'lg']),
+    'h-1' => $size === 'sm',
 ]) }}>
     <div class="w-full h-hull bg-gray-100 rounded-full overflow-hidden relative">
         <div class="bg-brand-500 absolute inset-y-0 left-0 transition-all ease-out" style="width: {{ $progress }}%;"></div>
