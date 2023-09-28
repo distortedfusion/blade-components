2672508d40769b14d51bebdb995904413d27278c
Kevin Dierkx <kevin@distortedfusion.com>
2023-09-29 00:55:44 +0200

    Pure css spinner

--- resources/views/components/layout/pulser.blade.php
+++ resources/views/components/layout/pulser.blade.php
@@ -1,7 +1,7 @@
 <div {{ $attributes->class([
-    'flex items-center space-x-1 animate-pulse',
+    'inline-flex items-center space-x-1',
 ]) }}>
-    <div class="w-1 h-1 rounded-full bg-current"></div>
-    <div class="w-1 h-1 rounded-full bg-current"></div>
-    <div class="w-1 h-1 rounded-full bg-current"></div>
+    <div class="w-1 h-1 rounded-full bg-current animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_0ms]"></div>
+    <div class="w-1 h-1 rounded-full bg-current animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_125ms]"></div>
+    <div class="w-1 h-1 rounded-full bg-current animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_250ms]"></div>
 </div>
--- resources/views/components/layout/spinner.blade.php
+++ resources/views/components/layout/spinner.blade.php
@@ -1,7 +1,10 @@
+@props(['size' => null])
 <div {{ $attributes->class([
-    'flex items-center justify-center',
-    'text-brand-500' => ! Str::contains($attributes->get('class'), 'text-'),
-    'p-4' => ! Str::contains($attributes->get('class'), ['p-', 'px-', 'py-']),
+    'inline-flex items-center aspect-square',
+
+    'w-8' => $size === 'lg',
+    'w-4' => is_null($size) || ! in_array($size, ['sm', 'lg']),
+    'w-2' => $size === 'sm',
 ]) }}>
-    <x-fad-spinner-third class="animate-spin" />
+    <div class="animate-spin w-full h-auto aspect-square border-2 border-black/20 border-t-brand-500 rounded-full"></div>
 </div>
