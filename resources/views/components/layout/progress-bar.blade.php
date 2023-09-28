7b6f7a0555037dc3cd3e19d8e86344d0e342d72f
Kevin Dierkx <kevin@distortedfusion.com>
2023-09-29 00:38:13 +0200

    bar rtypo

--- resources/views/components/layout/progress-bar.blade.php
+++ resources/views/components/layout/progress-bar.blade.php
@@ -1,9 +1,10 @@
 <div {{ $attributes->class([
+    'w-full',
     'h-4' => $size === 'lg',
     'h-2' => is_null($size) || ! in_array($size, ['sm', 'lg']),
     'h-1' => $size === 'sm',
 ]) }}>
-    <div class="w-full h-hull bg-gray-100 rounded-full overflow-hidden relative">
+    <div class="w-full h-full bg-gray-100 rounded-full overflow-hidden relative">
         <div class="bg-brand-500 absolute inset-y-0 left-0 transition-all ease-out" style="width: {{ $progress }}%;"></div>
     </div>
 </div>
