3e2339475a470743ba0ba6a29e6f9f38378ed6dc
Kevin Dierkx <kevin@distortedfusion.com>
2023-09-29 00:08:25 +0200

    Icon styling

--- resources/views/components/layout/icon.blade.php
+++ resources/views/components/layout/icon.blade.php
@@ -1,15 +1,18 @@
 <div {{ $attributes->class([
-    'inline-flex items-center justify-center rounded aspect-square',
+    'inline-flex items-center justify-center border rounded aspect-square',
 
     'w-12' => $size === 'lg',
     'w-10' => is_null($size) || ! in_array($size, ['sm', 'lg']),
     'w-8' => $size === 'sm',
 
     // Primary color...
-    'bg-brand-600 text-brand-100' => $style === 'primary',
+    'bg-brand-600 border-brand-600 text-brand-100' => $style === 'primary',
 
-    // Default color...
-    'bg-gray-600 text-gray-600' => $style === 'default',
+    // Secondary color...
+    'bg-white border-gray-300 text-brand-700' => $style === 'secondary',
+
+    // Tertiary color...
+    'bg-white border-black/10 text-gray-400' => $style === 'tertiary',
 ]) }}>
-    <x-dynamic-component :component="$icon" />
+    <x-dynamic-component :component="$icon" class="w-4 h-4" />
 </div>
