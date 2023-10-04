6179d96e190c2efd169085bc0732d9d249d017d1
Kevin Dierkx <kevin@distortedfusion.com>
2023-10-04 11:31:22 +0200

    Updated card contrast

--- resources/views/components/card/footer.blade.php
+++ resources/views/components/card/footer.blade.php
@@ -4,12 +4,12 @@
     'px-4 sm:px-6' => ! Str::contains($attributes->get('class'), ['px-']),
     'py-3' => ! Str::contains($attributes->get('class'), ['py-']),
 
-    'bg-gray-100/50 dark:bg-gray-900/50' => is_null($style) || ! in_array($style, ['warning', 'danger']),
+    'bg-gray-100 dark:bg-gray-900' => is_null($style) || ! in_array($style, ['warning', 'danger']),
     'bg-amber-50' => $style === 'warning',
     'bg-red-50' => $style === 'danger',
 
     'border-separate',
-    'border-gray-200 dark:border-gray-900' => is_null($style) || ! in_array($style, ['warning', 'danger']),
+    'border-gray-300 dark:border-gray-700' => is_null($style) || ! in_array($style, ['warning', 'danger']),
     'border-amber-200' => $style === 'warning',
     'border-red-200' => $style === 'danger',
 ]) }}>
