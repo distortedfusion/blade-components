9224bfa3314e91171246480df5a78c4c23b6357d
Kevin Dierkx <kevin@distortedfusion.com>
2023-10-04 11:27:27 +0200

    Updated dark style description

--- resources/views/components/text/description.blade.php
+++ resources/views/components/text/description.blade.php
@@ -1,6 +1,6 @@
 <dl {{ $attributes->class([
     'space-y-1',
 ]) }}>
-    <dt class="text-muted text-sm">{{ $title }}</dt>
+    <dt class="text-gray-600 dark:text-gray-400 text-sm">{{ $title }}</dt>
     <dd class="text-base">{{ $slot }}</dd>
 </dl>
