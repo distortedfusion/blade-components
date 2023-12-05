b7895367cc5ebd11ee7b53e170997761a6e52c9f
Kevin Dierkx <kevin@distortedfusion.com>
2023-12-05 16:19:09 +0100

    Spacing fix layout sections

--- resources/views/components/layout/section-group.blade.php
+++ resources/views/components/layout/section-group.blade.php
@@ -1,5 +1,6 @@
 <div {{ $attributes->class([
-    'space-y-8 divide-y divide-gray-200 dark:divide-gray-700',
+    'space-y-8 [&_section:not(:first-child)]:pt-8',
+    'divide-y divide-gray-200 dark:divide-gray-700',
 ]) }}>
     {{ $slot }}
 </div>
