64e9d5eca599868851a58b36ac91ff1284e596cd
Kevin Dierkx <kevin@distortedfusion.com>
2023-12-05 14:56:46 +0100

    Dropped section components

--- resources/views/components/layout/section-group.blade.php
+++
@@ -1,3 +0,0 @@
-<div {{ $attributes->merge(['class' => 'section-group space-y-8 divide-y divide-gray-200 dark:divide-gray-700']) }}>
-    {{ $slot }}
-</div>
--- resources/views/components/layout/section-title.blade.php
+++
@@ -1,1 +0,0 @@
-<h{{ $headingLevel }} {{ $attributes->class(['font-semibold']) }}>{{ $slot }}</h{{ $headingLevel }}>
