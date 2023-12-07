efb359bf1fbb61977c4923e0d8db3d2a84a37986
Kevin Dierkx <kevin@distortedfusion.com>
2023-12-07 21:42:31 +0100

    Updated title-bar markup

--- resources/views/components/layout/title-bar.blade.php
+++ resources/views/components/layout/title-bar.blade.php
@@ -1,36 +1,37 @@
-<header {{ $attributes->class([
-        'text-black dark:text-white',
-    ]) }}>
-    <x-dynamic-component :component="$currentRouteApplicableForNarrowLayout() ? 'container-narrow' : 'container'">
+<x-dynamic-component :component="$currentRouteApplicableForNarrowLayout() ? 'container-narrow' : 'container'">
+    <header {{ $attributes->class([
+            'py-6',
+            'text-black dark:text-white',
+            'border-b border-gray-300 dark:border-gray-700' => ! Str::contains($attributes->get('class'), 'border'),
+        ]) }}>
         <div class="space-y-6">
-            <div class="border-b border-gray-300 dark:border-gray-700 py-6">
-                <div class="flex items-center">
-                    @if($url = $getPreviousUrl())
-                        <div class="flex-shrink-0 border-r border-black/10 pr-2 mr-2">
-                            <x-btn :href="$url" size="sm" style="tertiary" class="px-2">
-                                <x-btn.icon icon="fal-chevron-left" />
-                            </x-btn>
-                        </div>
-                    @endif
-                    @if($icon)
-                        <div class="flex-shrink-0 mr-4">
-                            <x-layout.icon :icon="$icon" />
-                        </div>
-                    @endif
-                    <div class="flex-grow flex items-center space-x-4">
-                        <x-dynamic-component :component="$asHeading ? 'heading' : 'paragraph'"
-                            class="font-sans-heading font-bold text-lg leading-6 py-2">
-                            {{ $title }}
-                        </x-dynamic-component>
+            <div class="flex items-center">
+                @if($url = $getPreviousUrl())
+                    <div class="flex-shrink-0 border-r border-black/10 pr-2 mr-2">
+                        <x-btn :href="$url" size="sm" style="tertiary" class="px-2">
+                            <x-btn.icon icon="fal-chevron-left" />
+                        </x-btn>
                     </div>
-                    @if($actions ?? false)
-                        <div class="flex items-center space-x-2 ml-2">
-                            {!! $actions !!}
-                        </div>
-                    @endif
+                @endif
+                @if($icon)
+                    <div class="flex-shrink-0 mr-4">
+                        <x-layout.icon :icon="$icon" />
+                    </div>
+                @endif
+                <div class="flex-grow flex items-center space-x-4">
+                    <x-dynamic-component :component="$asHeading ? 'heading' : 'paragraph'"
+                        class="font-sans-heading font-bold text-lg leading-6 py-2">
+                        {{ $title }}
+                    </x-dynamic-component>
                 </div>
+                @if($actions ?? false)
+                    <div class="flex items-center space-x-2 ml-2">
+                        {!! $actions !!}
+                    </div>
+                @endif
             </div>
+
             {{ $slot }}
         </div>
-    </x-dynamic-component>
-</header>
+    </header>
+</x-dynamic-component>
