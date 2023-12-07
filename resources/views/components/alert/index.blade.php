da00768fa6e7a9488f5f74e85b8a7ebf36e3d0d6
Kevin Dierkx <kevin@distortedfusion.com>
2023-12-07 21:46:25 +0100

    Styling

--- resources/views/components/alert/index.blade.php
+++ resources/views/components/alert/index.blade.php
@@ -6,14 +6,16 @@
     'bg-yellow-50 border-yellow-100' => $type === 'warning',
     'bg-red-50 border-red-100' => $type === 'danger',
     'bg-gray-50 border-gray-100' => $type === 'default',
+    'dark:bg-gray-900 dark:border-gray-700' => $type === 'default',
 ]) }} role="alert">
     <div {{ (new \Illuminate\View\ComponentAttributeBag)->class([
-        'h-full my-1 p-2 rounded flex-shrink-0 inline-block',
-        'bg-green-100 text-green-600' => $type === 'success',
-        'bg-blue-100 text-blue-600' => $type === 'info',
-        'bg-yellow-100 text-yellow-600' => $type === 'warning',
-        'bg-red-100 text-red-600' => $type === 'danger',
-        'bg-gray-100 text-gray-600' => $type === 'default',
+        'h-full my-1 p-2 flex-shrink-0 inline-block',
+        'text-green-600' => $type === 'success',
+        'text-blue-600' => $type === 'info',
+        'text-yellow-600' => $type === 'warning',
+        'text-red-600' => $type === 'danger',
+        'text-gray-700' => $type === 'default',
+        'dark:text-gray-300' => $type === 'default',
     ]) }}>
         <x-dynamic-component :component="$icon" class="w-4 h-4" />
     </div>
