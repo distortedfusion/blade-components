926b2e8f22e94cf31f2a2a7cb2c880ec7bee4241
Kevin Dierkx <kevin@distortedfusion.com>
2024-03-06 13:41:22 +0100

    x-p xl option

--- resources/views/components/text/paragraph.blade.php
+++ resources/views/components/text/paragraph.blade.php
@@ -1,8 +1,9 @@
 <p {{ $attributes->class([
     'text-gray-600 dark:text-gray-300',
 
+    'text-xl' => $size === 'xl',
     'text-base' => $size === 'lg',
-    'text-sm' => is_null($size) || ! in_array($size, ['lg']),
+    'text-sm' => is_null($size) || ! in_array($size, ['lg', 'xl']),
 ]) }}>
     {{ $slot }}
 </p>
