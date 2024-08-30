9829253280712eec7dc551c97a70d8a4f6933e2b
Kevin Dierkx <kevin@distortedfusion.com>
2024-08-30 13:54:26 +0200

    action width and card footer

--- resources/views/components/card/footer.blade.php
+++ resources/views/components/card/footer.blade.php
@@ -5,11 +5,11 @@
     'py-3' => ! Str::contains($attributes->get('class'), ['py-']),
 
     'border-t',
-    'bg-black/5 dark:bg-white/5 border-black/10 dark:border-white/10' => $style === 'default',
-    'bg-green-50/95 dark:bg-green-500/10 border-green-500/50 dark:border-green-500/50' => $style === 'success',
-    'bg-blue-50/95 dark:bg-blue-500/10 border-blue-500/50 dark:border-blue-500/50' => $style === 'info',
-    'bg-amber-50/95 dark:bg-amber-500/10 border-amber-500/50 dark:border-amber-500/50' => $style === 'warning',
-    'bg-red-50/95 dark:bg-red-500/10 border-red-500/50 dark:border-red-500/50' => $style === 'danger',
+    'dark:border-white/10' => $style === 'default',
+    'border-green-500/50 dark:border-green-500/50' => $style === 'success',
+    'border-blue-500/50 dark:border-blue-500/50' => $style === 'info',
+    'border-amber-500/50 dark:border-amber-500/50' => $style === 'warning',
+    'border-red-500/50 dark:border-red-500/50' => $style === 'danger',
 ]) }}>
     {{ $slot }}
 </div>
--- resources/views/components/form/actions.blade.php
+++ resources/views/components/form/actions.blade.php
@@ -1,11 +1,11 @@
 <div {{ $attributes->class([
-        'flex flex-col flex-col-reverse sm:flex-row items-center sm:space-x-4',
+        'w-full flex flex-col flex-col-reverse sm:flex-row items-center sm:space-x-4',
     ]) }}>
     <div class="flex-1 w-full mt-2 sm:mt-0">
         @if($cancelButton ?? false)
             {{ $cancelButton }}
         @else
-            <x-btn style="default" class="w-full" :href="$cancelUrl()">
+            <x-btn style="secondary" class="w-full" :href="$cancelUrl()">
                 {{ $cancelTitle ?: __('Cancel') }}
             </x-btn>
         @endif
