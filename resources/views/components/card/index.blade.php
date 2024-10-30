c4aa1f32d4eaa4422aae6f3d093a60e9fee0ec07
Kevin Dierkx <kevin@distortedfusion.com>
2024-10-30 23:49:17 +0100

    borderless cards

--- resources/views/components/card/index.blade.php
+++ resources/views/components/card/index.blade.php
@@ -3,7 +3,6 @@ use Illuminate\Support\Str;
 @endphp
 <div {{ $attributes->class([
     'card rounded-lg',
-    'border border-black/10 dark:border-white/10' => ! Str::contains($attributes->get('class'), ['border-']),
     'bg-white dark:bg-gray-950' => ! Str::contains($attributes->get('class'), ['bg-']),
     'ring-2 ring-offset-2 ring-offset-white dark:ring-offset-black ring-green-500' => $style === 'success',
     'ring-2 ring-offset-2 ring-offset-white dark:ring-offset-black ring-blue-500' => $style === 'info',
