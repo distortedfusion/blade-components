b26c0e650107be7f9218b6c683d008ef90126649
Kevin Dierkx <kevin@distortedfusion.com>
2024-10-30 23:10:02 +0100

    Optional bg and border on cards

--- resources/views/components/card/index.blade.php
+++ resources/views/components/card/index.blade.php
@@ -1,7 +1,10 @@
+@php
+use Illuminate\Support\Str;
+@endphp
 <div {{ $attributes->class([
     'card rounded-lg',
-    'border border-black/10 dark:border-white/10',
-    'bg-white dark:bg-white/5',
+    'border border-black/10 dark:border-white/10' => ! Str::contains($attributes->get('class'), ['border-']),
+    'bg-white dark:bg-white/5' => ! Str::contains($attributes->get('class'), ['bg-']),
     'ring-2 ring-offset-2 ring-offset-white dark:ring-offset-black ring-green-500' => $style === 'success',
     'ring-2 ring-offset-2 ring-offset-white dark:ring-offset-black ring-blue-500' => $style === 'info',
     'ring-2 ring-offset-2 ring-offset-white dark:ring-offset-black ring-amber-500' => $style === 'warning',
