08bbd9614e1142c7677e5050bed123aaebf59f1b
Kevin Dierkx <kevin@distortedfusion.com>
2024-10-30 23:37:53 +0100

    Updated card styling

--- resources/views/components/card/index.blade.php
+++ resources/views/components/card/index.blade.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Str;
 <div {{ $attributes->class([
     'card rounded-lg',
     'border border-black/10 dark:border-white/10' => ! Str::contains($attributes->get('class'), ['border-']),
-    'bg-white dark:bg-white/5' => ! Str::contains($attributes->get('class'), ['bg-']),
+    'bg-white dark:bg-gray-950' => ! Str::contains($attributes->get('class'), ['bg-']),
     'ring-2 ring-offset-2 ring-offset-white dark:ring-offset-black ring-green-500' => $style === 'success',
     'ring-2 ring-offset-2 ring-offset-white dark:ring-offset-black ring-blue-500' => $style === 'info',
     'ring-2 ring-offset-2 ring-offset-white dark:ring-offset-black ring-amber-500' => $style === 'warning',
