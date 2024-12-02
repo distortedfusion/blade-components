ddd55df20334e29fe04ca1f5501a1bec2dcb6763
Kevin Dierkx <kevin@distortedfusion.com>
2024-12-02 22:30:21 +0100

    Fixed progress-bar

--- resources/views/components/layout/progress-bar.blade.php
+++ resources/views/components/layout/progress-bar.blade.php
@@ -4,7 +4,7 @@
     'h-2' => is_null($size) || ! in_array($size, ['sm', 'lg']),
     'h-1' => $size === 'sm',
 ]) }}>
-    <div class="w-full h-full bg-gray-100 rounded-full overflow-hidden relative">
-        <div class="bg-brand-500 absolute inset-y-0 left-0 transition-all ease-out" style="width: {{ $progress }}%;"></div>
+    <div class="w-full h-full bg-black/10 dark:bg-white/10 rounded-full overflow-hidden relative">
+        <div class="bg-[rgb(var(--primary-500))] absolute inset-y-0 left-0 transition-all ease-out" style="width: {{ $progress }}%;"></div>
     </div>
 </div>
