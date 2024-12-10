e3426988239fa002a051bcb63c23605794b31870
Kevin Dierkx <kevin@distortedfusion.com>
2024-12-10 23:26:58 +0100

    Removed unused classes

--- resources/views/components/entity/field.blade.php
+++ resources/views/components/entity/field.blade.php
@@ -1,5 +1,5 @@
 <div {{ $attributes }}>
-    <x-heading :as-heading="false" size="flex" class="text-sm min-w-0 truncate">{{ $slot }}</x-heading>
+    <x-heading :as-heading="false" size="flex" class="text-sm truncate">{{ $slot }}</x-heading>
     @if($description ?? false)
         <div {{ $description->attributes }}>
             {{ $description }}
