0cf46005df8884ad856ff6bbad37d26bdfaae930
Kevin Dierkx <kevin@distortedfusion.com>
2025-01-19 22:20:17 +0100

    Included space in check

--- resources/views/components/layout/container.blade.php
+++ resources/views/components/layout/container.blade.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Str;
 {{-- The max-width, except for 3xl, are matched to the default tailwindcss breakpoints --}}
 <div {{ $attributes->class([
     'mx-auto',
-    'px-4 sm:px-6 lg:px-8' => ! Str::contains($attributes->get('class'), ['p-', 'px-']),
+    'px-4 sm:px-6 lg:px-8' => ! Str::contains($attributes->get('class'), [' p-', ' px-']),
     'max-w-full' => ($size === 'full'),
     'max-w-[120rem]' => ($size === '3xl'),
     'max-w-[96rem]' => ($size === '2xl'),
