6d10c076a53d63f624fd6360ad896699e2c919a1
Kevin Dierkx <kevin@distortedfusion.com>
2025-01-22 13:09:43 +0100

    Updated pre spacing

--- resources/views/components/text/pre.blade.php
+++ resources/views/components/text/pre.blade.php
@@ -1,5 +1,7 @@
 @props(['withCopy' => false, 'copyLabel' => 'Copy', 'copiedLabel' => 'Copied!'])
 @php
+use Illuminate\View\ComponentAttributeBag;
+
 $refId = 'pre-'.crc32($slot);
 @endphp
 <div {{ $attributes->class([
@@ -11,16 +13,16 @@ $refId = 'pre-'.crc32($slot);
         'copyLabel': '{{ $copyLabel }}',
         'copiedLabel': '{{ $copiedLabel }}',
     }">
-    <pre x-ref="{{ $refId }}" {{ $attributes->class([
-        'flex-grow p-4 overflow-scroll',
+    <pre x-ref="{{ $refId }}" {{ (new ComponentAttributeBag())->class([
+        'flex-grow px-4 py-2 overflow-scroll',
         'text-sm leading-6 font-mono',
     ]) }}>{{ $slot }}</pre>
     @if($withCopy)
-        <div class="flex-shrink-0 pr-3">
-            <x-btn class="mt-3"
-                style="tertiary"
-                size="sm"
-                x-on:click="navigator.clipboard.writeText($refs[refId].innerText); $el.textContent = copiedLabel; setTimeout(() => $el.textContent = copyLabel, 2000)">
+        <div {{ (new ComponentAttributeBag())->class([
+            'flex-shrink-0 pr-1 relative',
+            'pt-1',
+        ]) }}>
+            <x-btn style="tertiary" size="sm" x-on:click="navigator.clipboard.writeText($refs[refId].innerText); $el.textContent = copiedLabel; setTimeout(() => $el.textContent = copyLabel, 2000)">
                 {{ $copyLabel }}
             </x-btn>
         </div>
