8c743ab36c109a81045a6c744ab46636884eb0b9
Kevin Dierkx <kevin@distortedfusion.com>
2025-01-29 16:20:21 +0100

    Dark mode spinner

--- resources/views/components/spinner/index.blade.php
+++ resources/views/components/spinner/index.blade.php
@@ -6,5 +6,5 @@
     'w-4' => is_null($size) || ! in_array($size, ['sm', 'lg']),
     'w-2' => $size === 'sm',
 ]) }}>
-    <div class="animate-spin w-full h-auto aspect-square border-2 border-black/20 border-t-brand-500 rounded-full"></div>
+    <div class="animate-spin w-full h-auto aspect-square border-2 border-black/10 dark:border-white/10 border-t-black dark:border-t-white rounded-full"></div>
 </div>
