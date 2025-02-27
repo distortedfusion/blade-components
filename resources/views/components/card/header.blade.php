31069804825d17de7a6c606610bf1ad42a91b066
Kevin Dierkx <kevin@distortedfusion.com>
2025-02-28 00:08:20 +0100

    Wrapped card header slot

--- resources/views/components/card/header.blade.php
+++ resources/views/components/card/header.blade.php
@@ -1,12 +1,14 @@
 <div data-slot="card-header" {{ $attributes->class([
-    'flex items-center justify-between space-x-2 rounded-t-lg',
+    'flex items-center space-x-2 rounded-t-lg',
     'p-4 sm:px-6',
 
     // Heading styling...
-    '[&>[data-slot=heading]]:text-base [&>[data-slot=heading]]:py-1',
-    '[&>*:is(h1,h2,h3,h4,h5,h6)]:text-base [&>*:is(h1,h2,h3,h4,h5,h6)]:py-1',
+    '[&_[data-slot=heading]]:text-base [&_[data-slot=heading]]:py-1',
+    '[&_*:is(h1,h2,h3,h4,h5,h6)]:text-base [&_*:is(h1,h2,h3,h4,h5,h6)]:py-1',
 ]) }}>
-    {{ $slot }}
+    <div class="flex-grow">
+        {{ $slot }}
+    </div>
     @if($action ?? false)
         <div class="flex-shrink-0 flex items-center space-x-2">
             {{ $action }}
