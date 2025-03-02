0815a353c36667feb1f4f599a8c9d7f0afd1ad28
Kevin Dierkx <kevin@distortedfusion.com>
2025-03-02 21:32:39 +0100

    Mute entity.field

--- resources/views/components/entity/field.blade.php
+++ resources/views/components/entity/field.blade.php
@@ -1,7 +1,9 @@
 <div {{ $attributes }}>
     <x-heading :as-heading="false" size="flex" class="text-sm truncate">{{ $slot }}</x-heading>
     @if($description ?? false)
-        <div {{ $description->attributes }}>
+        <div {{ $description->attributes->class([
+            '[&_p]:text-gray-600 dark:[&_p]:text-gray-400',
+        ]) }}>
             {{ $description }}
         </div>
     @endif
