875c0854c9b42ff22a1d03324a50c909bccff0a3
Kevin Dierkx <kevin@distortedfusion.com>
2025-03-02 22:33:19 +0100

    Give empty states some breathing room

--- resources/views/components/layout/empty-state.blade.php
+++ resources/views/components/layout/empty-state.blade.php
@@ -1,4 +1,6 @@
-<div {{ $attributes->class(['text-center']) }}>
+<div {{ $attributes->class([
+    'text-center py-12',
+]) }}>
     <x-heading size="flex" :heading-level="3" :as-heading="$asHeading" class="text-base leading-6 text-black dark:text-white py-2">{{ $title }}</x-heading>
     <div class="text-sm space-y-4 [&_p]:text-gray-600 dark:[&_p]:text-gray-400">
         {{ $slot }}
