aa1e649c6ab3cecc2d47f86d2e89a0f41b206920
Kevin Dierkx <kevin@distortedfusion.com>
2025-03-02 22:38:45 +0100

    Max char width on empty state content

--- resources/views/components/layout/empty-state.blade.php
+++ resources/views/components/layout/empty-state.blade.php
@@ -1,8 +1,8 @@
 <div {{ $attributes->class([
-    'text-center py-12',
+    'flex flex-col items-center text-center py-12',
 ]) }}>
     <x-heading size="flex" :heading-level="3" :as-heading="$asHeading" class="text-base leading-6 text-black dark:text-white py-2">{{ $title }}</x-heading>
-    <div class="text-sm space-y-4 [&_p]:text-gray-600 dark:[&_p]:text-gray-400">
+    <div class="max-w-[64ch] text-sm space-y-4 [&_p]:text-gray-600 dark:[&_p]:text-gray-400">
         {{ $slot }}
     </div>
 
