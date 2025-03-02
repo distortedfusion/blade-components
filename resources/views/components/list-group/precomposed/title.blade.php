20e34fa7d8308c60ffaf22fd6c3206cd3e53140a
Kevin Dierkx <kevin@distortedfusion.com>
2025-03-02 21:25:19 +0100

    Mute list-group.item title

--- resources/views/components/list-group/precomposed/title.blade.php
+++ resources/views/components/list-group/precomposed/title.blade.php
@@ -2,7 +2,7 @@
     'flex flex-wrap flex-col md:flex-row items-start',
 ]) }}>
     <div class="md:w-32 md:flex-shrink-0 md:mr-2">
-        <span class="font-semibold text-gray-800 dark:text-gray-100">{!! $title !!}</span>
+        <span class="font-semibold text-gray-600 dark:text-gray-400">{!! $title !!}</span>
     </div>
 
     {{ $slot }}
