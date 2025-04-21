bf36acc54e27203d7d3f49b503359e3691db8520
Kevin Dierkx <kevin@distortedfusion.com>
2025-04-21 16:32:42 +0200

    Attribute support separator

--- resources/views/components/separator/index.blade.php
+++ resources/views/components/separator/index.blade.php
@@ -2,7 +2,7 @@
 @php
 use Illuminate\View\ComponentAttributeBag;
 @endphp
-<div data-slot="separator" data-orientation="{{ $vertical ? 'vertical' : 'horizontal' }}" {{ (new ComponentAttributeBag)->class([
+<div data-slot="separator" data-orientation="{{ $vertical ? 'vertical' : 'horizontal' }}" {{ $attributes->class([
     'self-center self-stretch',
     'border-0 bg-black/10 dark:bg-white/10',
     'h-px' => ! $vertical,
