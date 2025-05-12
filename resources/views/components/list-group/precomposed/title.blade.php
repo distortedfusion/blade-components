f1695e0939d5ab06faaa042ffa3ca2698d5e73d2
Kevin Dierkx <kevin@distortedfusion.com>
2025-05-12 11:52:05 +0200

    Updated list group title

--- resources/views/components/list-group/precomposed/title.blade.php
+++ resources/views/components/list-group/precomposed/title.blade.php
@@ -2,7 +2,7 @@
     'flex flex-wrap flex-col md:flex-row items-start',
 ]) }}>
     <div class="md:w-32 md:flex-shrink-0 md:mr-2">
-        <span class="font-semibold text-[var(--muted-foreground)]">{!! $title !!}</span>
+        <x-heading :heading-level="3" size="sm">{!! $title !!}</x-heading>
     </div>
 
     {{ $slot }}
