419e274d2fe0a33dd379618dfdc2e6aa8bb28790
Kevin Dierkx <kevin@distortedfusion.com>
2025-05-12 14:08:52 +0200

    Removed flex from card footer

--- resources/views/components/card/footer.blade.php
+++ resources/views/components/card/footer.blade.php
@@ -1,5 +1,5 @@
 <div data-slot="card-footer" {{ $attributes->class([
-    'flex items-center gap-x-2 rounded-b-[var(--radius-inner)]',
+    'rounded-b-[var(--radius-inner)]',
     'px-4 sm:px-6' => ! Str::contains($attributes->get('class'), ['px-']),
     'py-3' => ! Str::contains($attributes->get('class'), ['py-']),
 ]) }}>
