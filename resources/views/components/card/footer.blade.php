7d54b759f8f3cd5121066b8e0b3509b95ada9995
Kevin Dierkx <kevin@distortedfusion.com>
2025-06-17 17:09:28 +0200

    Reintroduced card-footer flex

--- resources/views/components/card/footer.blade.php
+++ resources/views/components/card/footer.blade.php
@@ -1,4 +1,5 @@
 <div data-slot="card-footer" {{ $attributes->class([
+    'flex flex-col-reverse sm:flex-row sm:justify-end gap-2',
     'rounded-b-[var(--radius-inner)]',
     'px-4 sm:px-6' => ! Str::contains($attributes->get('class'), ['px-']),
     'py-3' => ! Str::contains($attributes->get('class'), ['py-']),
