fcd629139e0ac384fef03bab2232ef127d758bc1
Kevin Dierkx <kevin@distortedfusion.com>
2025-09-17 23:36:00 +0200

    Icon scale

--- resources/views/components/layout/icon.blade.php
+++ resources/views/components/layout/icon.blade.php
@@ -1,11 +1,14 @@
 @props(['icon', 'style' => 'primary', 'size' => null])
+@php
+use Illuminate\View\ComponentAttributeBag;
+@endphp
 <div {{ $attributes->class([
     'inline-flex items-center justify-center border rounded-md aspect-square',
 
-    'w-12' => $size === 'lg',
-    'w-10' => is_null($size) || ! in_array($size, ['xs', 'sm', 'lg']),
-    'w-8' => $size === 'sm',
-    'w-6' => $size === 'xs',
+    '[&>[data-slot=icon]]:size-4 w-12' => $size === 'lg',
+    '[&>[data-slot=icon]]:size-4 w-10' => is_null($size),
+    '[&>[data-slot=icon]]:size-4 w-8' => $size === 'sm',
+    '[&>[data-slot=icon]]:size-3 w-6' => $size === 'xs',
 
     // Primary...
     'bg-[var(--primary)] border-transparent text-[var(--primary-foreground)]' => $style === 'primary',
@@ -16,5 +19,5 @@
     // Outline...
     'bg-transparent border-[var(--border)] text-[var(--secondary-foreground)]' => $style === 'outline',
 ]) }}>
-    <x-dynamic-component :component="$icon" class="size-4" />
+    <x-dynamic-component :component="$icon" data-slot="icon" />
 </div>
