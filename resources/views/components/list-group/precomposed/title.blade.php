290bbd3d72bea5436d6b0540d44366165b21f0fa
Kevin Dierkx <kevin@distortedfusion.com>
2025-09-21 20:52:50 +0200

    List group title styling

--- resources/views/components/list-group/precomposed/title.blade.php
+++ resources/views/components/list-group/precomposed/title.blade.php
@@ -2,7 +2,7 @@
     'flex flex-wrap flex-col md:flex-row items-start',
 ]) }}>
     <div class="md:w-32 md:flex-shrink-0 md:mr-2">
-        <x-heading :heading-level="3" size="sm">{!! $title !!}</x-heading>
+        <x-paragraph size="sm" style="muted">{!! $title !!}</x-paragraph>
     </div>
 
     {{ $slot }}
