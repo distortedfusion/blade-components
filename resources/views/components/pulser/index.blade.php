4d8f4beb2ed427bccf755b7373085d41c029858a
Kevin Dierkx <kevin@distortedfusion.com>
2025-10-16 16:11:28 +0200

    Added dot variant and styles to pulser

--- resources/views/components/pulser/index.blade.php
+++ resources/views/components/pulser/index.blade.php
@@ -1,7 +1,23 @@
+@props(['style' => 'default', 'variant' => null])
 <div {{ $attributes->class([
-    'inline-flex items-center gap-x-1',
+    'inline-block',
+
+    '[&_[data-slot=pulser-dot]]:bg-[var(--primary)]' => $style === 'default',
+    '[&_[data-slot=pulser-dot]]:bg-[var(--success)]' => $style === 'success',
+    '[&_[data-slot=pulser-dot]]:bg-[var(--info)]' => $style === 'info',
+    '[&_[data-slot=pulser-dot]]:bg-[var(--warning)]' => $style === 'warning',
+    '[&_[data-slot=pulser-dot]]:bg-[var(--danger)]' => $style === 'danger',
 ]) }}>
-    <div class="size-1 rounded-full bg-[var(--foreground)] animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_0ms]"></div>
-    <div class="size-1 rounded-full bg-[var(--foreground)] animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_125ms]"></div>
-    <div class="size-1 rounded-full bg-[var(--foreground)] animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_250ms]"></div>
+    @if($variant !== 'dot')
+        <div class="inline-flex items-center gap-x-1">
+            <div data-slot="pulser-dot" class="size-1 rounded-full animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_0ms]"></div>
+            <div data-slot="pulser-dot" class="size-1 rounded-full animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_125ms]"></div>
+            <div data-slot="pulser-dot" class="size-1 rounded-full animate-[pulse_2s_infinite_cubic-bezier(.4,0,.6,1)_250ms]"></div>
+        </div>
+    @else
+        <div class="h-2 w-2 relative">
+            <div data-slot="pulser-dot" class="h-full w-full rounded-full relative"></div>
+            <div data-slot="pulser-dot" class="h-full w-full rounded-full absolute top-0 left-0 opacity-75 animate-ping"></div>
+        </div>
+    @endif
 </div>
