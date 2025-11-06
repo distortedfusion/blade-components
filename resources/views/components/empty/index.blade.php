29ec25e3461b10376aa86344cdd62c3facdcdf7f
Kevin Dierkx <kevin@distortedfusion.com>
2025-11-06 13:43:56 +0100

    Empty content styling

--- resources/views/components/empty/index.blade.php
+++ resources/views/components/empty/index.blade.php
@@ -2,7 +2,11 @@
 <div data-slot="empty" {{ $attributes->class([
     'min-w-0 flex flex-1 flex-col items-center justify-center gap-6 p-6 md:p-12 rounded-[var(--radius)]',
     'border border-[var(--border)] border-dashed',
+
     'text-center text-balance',
+
+    '[&_p]:text-[var(--muted-foreground)] [&_p]:text-sm',
+    '[&_a:hover:not([data-slot=button])]:text-[var(--primary)] [&_a:not([data-slot=button])]:underline [&_a:not([data-slot=button])]:underline-offset-4',
 ]) }}>
     <div data-slot="empty-header" class="flex max-w-md flex-col items-center gap-2 text-center [&>[data-slot=heading]:not(:first-child)]:mt-2">
         @if(is_string($icon) && ! is_null($icon))
