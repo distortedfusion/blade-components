43057014adf96e9736e61a9eacce5ce92652214a
Kevin Dierkx <kevin@distortedfusion.com>
2025-12-30 16:26:07 +0100

    Corrected btn.group rounding

--- resources/views/components/btn/group.blade.php
+++ resources/views/components/btn/group.blade.php
@@ -2,7 +2,7 @@
     'flex items-center',
     '[&>[data-slot=button]:not(:first-child)]:rounded-l-none',
     '[&>[data-slot=button]:not(:first-child)]:border-l-0',
-    '[&>[data-slot=button]:first-child:not(:last-child)]:rounded-r-none',
+    '[&>[data-slot=button]:not(:last-child)]:rounded-r-none',
 ]) }}>
     {{ $slot }}
 </div>
