f45224cdf4a66011643267ce5c8546129d14e7bc
Kevin Dierkx <kevin@distortedfusion.com>
2026-08-18 21:16:21 +0200

    Cleanup

--- resources/views/components/layout/sidebar-backdrop.blade.php
+++ resources/views/components/layout/sidebar-backdrop.blade.php
@@ -1,10 +1,3 @@
-@php
-$classes = Flux::classes()
-    ->add('z-20 fixed inset-0 bg-black/10 hidden')
-    ->add('data-flux-sidebar-on-mobile:not-data-flux-sidebar-collapsed-mobile:block')
-    ;
-@endphp
-
 <ddfsn-sidebar-toggle data-slot="sidebar-backdrop" {{ $attributes->class([
     'z-20 fixed inset-0 bg-[var(--background)]/40 hidden',
     'data-ddfsn-sidebar-breakpoint-down:not-data-ddfsn-sidebar-collapsed:block',
